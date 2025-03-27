dc8749ff750ccf2911832f76cf784ce4b19839fe
Yael Axelrad <yael432a@email.com>
2025-03-28 01:44:26 +0200

    תיקון import של data מ-react-router-dom

--- src/Components/CustomWorkoutStart.jsx
+++ src/Components/CustomWorkoutStart.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { data, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import "./GymLandingPage.css";
 import {
   Typography,
